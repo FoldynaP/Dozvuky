567dadf13c721e54583159679db0a3cc873b993e
Petr Foldyna <petr.foldyna@bluesoft.cz>
2023-05-07 22:13:38 +0200

    fixed responsive menu navigation

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -7,9 +7,14 @@ export const init = () => {
     function toggleMenu(e) {
         const body = document.querySelector("body");
         const parent = e.target.closest(".js-menu");
+        const links = parent.querySelectorAll(".js-menu-link");
         const content = parent.querySelector(".js-menu-content");
+        console.log(e)
         content.classList.toggle("is-open");
         body.classList.toggle("menu-open");
+        links.forEach(link => {
+            link.addEventListener("click", toggleMenu)
+        });
     }
 
     menuBtn.addEventListener("click", toggleMenu);
